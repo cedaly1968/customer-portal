5cb1f191a51cf1ff93c76bd51e10892b5fe0efc0
Chris Daly <cedaly@dashfacts.com>
2014-05-03 00:41:17 -0400

    Adding error log files

---
+++ portal/var/log/x-errors_log-140503.php
@@ -0,0 +1,10 @@
+<?php die(); 
+// Should you require our technical assistance with the error logs troubleshooting feel free to contact us through the
+// personal HelpDesk at https://secure.x-cart.com/customer.php or email us on support@x-cart.com
+//
+// Technical support service can be purchased at http://www.x-cart.com/technical-support.html
+?>
+[03-May-2014 00:21:56] LOG message:
+    '/home/ced1968/public_html/portal/var/log/x-errors_php-140503.php' logfile has been created. remote ip '23.116.52.133'
+Request URI: /dispatcher.php
+-------------------------------------------------
---
+++ portal/var/log/x-errors_php-140503.php
@@ -0,0 +1,16 @@
+<?php die(); 
+// Should you require our technical assistance with the error logs troubleshooting feel free to contact us through the
+// personal HelpDesk at https://secure.x-cart.com/customer.php or email us on support@x-cart.com
+//
+// Technical support service can be purchased at http://www.x-cart.com/technical-support.html
+?>
+[03-May-2014 00:21:56] Warning: Invalid argument supplied for foreach() in /home/ced1968/public_html/portal/modules/HighCharts/customer/hc_customer_chart.php on line 9 
+
+[03-May-2014 00:21:56] Warning: Invalid argument supplied for foreach() in /home/ced1968/public_html/portal/modules/HighCharts/customer/hc_customer_chart.php on line 24 
+
+[03-May-2014 00:21:56] Warning: Smarty error: unable to read resource: "customer/dialog_message.tpl" in /home/ced1968/public_html/portal/include/lib/smarty/Smarty.class.php on line 1093 
+
+[03-May-2014 00:21:59] Warning: Smarty error: unable to read resource: "customer/dialog_message.tpl" in /home/ced1968/public_html/portal/include/lib/smarty/Smarty.class.php on line 1093 
+
+[03-May-2014 00:22:01] Warning: Smarty error: unable to read resource: "customer/dialog_message.tpl" in /home/ced1968/public_html/portal/include/lib/smarty/Smarty.class.php on line 1093 
+
